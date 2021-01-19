fa8de6d6109ce88cc1d8543c85900360b7860af8
sublimacja <zuzanna.adamiec@gmail.com>
2021-01-19 21:07:05 +0100

    generowanie gettera w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,6 +8,10 @@ public class DefaultCountingOutRhymer {
 
     public int total = INT;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
