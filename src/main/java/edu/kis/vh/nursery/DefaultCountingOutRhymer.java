1ea955ef9c5e3055e3640a009c4b8cd5089abeae
sublimacja <zuzanna.adamiec@gmail.com>
2021-01-19 21:19:00 +0100

    test

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -38,4 +38,5 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+
 }
--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -14,6 +14,7 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
         while (!temp.callCheck())
             countIn(temp.countOut());
 
+        //komentarz
         return ret;
     }
 }
